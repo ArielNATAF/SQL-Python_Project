c59c6d1024984dd2291d52b2bdbb1e71c80c28a4
Ariel NATAF <31856651+ArielNATAF@users.noreply.github.com>
2021-03-24 21:33:37 +0100

    final

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ import pandas as pd
 from os import path
 
 
+# Function for reading the user data base login information so they are not hardcoded in the code.
+def read_login_db():
+    if not path.exists("./login.json"):
+        print("missing \"login.json\" file with login information in directory",
+              "\nWrite data with format [{\"server\":\"SQLSERVER\\NAME\",\"db_name\":\"Survey_Sample_A19\"}]")
+        exit()
+
+    logs = pd.read_json("./login.json", orient='records')
+    server = logs.loc[0]['server']
+    db = logs.loc[0]['db_name']
+    return server, db
+
+
+# Connect to db, display error if can not
 def connect_to_db(server, db):
     try:
         conn = pyodbc.connect('Driver={SQL Server};'
@@ -11,11 +25,15 @@ def connect_to_db(server, db):
                                                                       'Trusted_Connection=yes;')
         print("connected to [" + server + "][" + db + "]")
     except pyodbc.Error as err:
-        raise SystemExit("/!\ Connection failed. error:\n%s" % err.args[1])
+        raise SystemExit("/!\ Connection failed. error:\n%s\nBe sure login.json is correctly filed" % err.args[1])
     return conn
 
 
 def get_db_struct(conn):
+    # Trying to reconstitute a table with all questions in all survey
+    # the table in db used to join is incomplete, it doesn't include question not asked
+    # (q4 is absent even though it is present in the expected result shown in class
+
     def cursor_struct_query(survey_id):
         # "1 AS IsAsked" when in the survey the question is asked, 0 when not
         query = ('SELECT DISTINCT s.SurveyId, q.QuestionId, sum( '
@@ -24,12 +42,12 @@ def get_db_struct(conn):
                  '      ELSE 0  '
                  '    END) '
                  'AS IsAsked '
-                 'FROM Question q, SurveyStructure s '
+                 'FROM [Question] q, [SurveyStructure] s '
                  'WHERE s.SurveyId = %d '
                  'GROUP BY SurveyId, q.QuestionId') % survey_id
         return query
 
-    # Table SurveyStructure only include Questions referenced, for instance question 4 is missing
+    # Table SurveyStructure only include Answers referenced, for instance Answer 4 is missing
     # A first cursor to get the structure by survey
     c1 = conn.cursor()
     c1.execute("SELECT SurveyId FROM Survey ORDER BY SurveyId")
@@ -47,12 +65,14 @@ def get_db_struct(conn):
     return df
 
 
+# Generate the pivot query, works on two level, a subquery for the "SELECT level" with union of multiple table
+# and "column level" checking if user has answered a question of a survey thanks to COALESCE
 def set_pivot_query(db_struct):
     def set_query_column_level(id_survey, id_question, is_asked, id_question_max):
         if is_asked != 0:
             query_col = ("COALESCE (\n"
                          "    (   SELECT a.Answer_Value \n"
-                         "        FROM Answer a \n"
+                         "        FROM [Answer] a \n"
                          "        WHERE a.UserId = u.UserId \n"
                          "        AND a.SurveyId = %d AND a.QuestionId = %d \n"
                          "    ), NULL) AS Answer_%d\n") % (id_survey, id_question, id_question)
@@ -64,7 +84,7 @@ def set_pivot_query(db_struct):
 
     def set_query_select_level(id_survey, column_query, id_min):
         query_select = ("SELECT UserId, %s as SurveyId, %s "
-                        "\nFROM [User] as u WHERE EXISTS (SELECT * FROM Answer as a "
+                        "\nFROM [User] as u \nWHERE EXISTS (SELECT * FROM [Answer] AS a "
                         "WHERE u.UserId = a.UserId AND a.SurveyId = %s)") % (id_survey, column_query, id_survey)
         if id_survey != id_min:
             query_select = " \nUNION " + query_select
@@ -77,33 +97,31 @@ def set_pivot_query(db_struct):
     for survey_id in survey_ids:
         mid_query = ""
         for question_id in question_ids:
-            # We check if for this survey, the question is asked and get the appropriate part of the query
+            # Check if for this survey the question is asked
             is_asked_id = db_struct[(db_struct["QuestionId"] == question_id) &
                                     (db_struct["SurveyId"] == survey_id)].loc[:, "IsAsked"].values
             mid_query += set_query_column_level(survey_id, question_id, is_asked_id, question_ids.max())
         query += set_query_select_level(survey_id, mid_query, survey_ids.min())
+    print(query)
     return query
 
 
+# Query to get the data from the answers
 def get_all_data_from_answer():
     query = ('SELECT a.*, u.User_Name, u.User_Email, '
              'q.Question_Text,s.SurveyDescription, s.Survey_UserAdminId '
-             'FROM  [Survey_Sample_A19].[dbo].[Answer] As a '
-             'INNER JOIN [Survey_Sample_A19].[dbo].[User] As u '
+             'FROM  [Survey_Sample_A19].[dbo].[Answer] AS a '
+             'INNER JOIN [Survey_Sample_A19].[dbo].[User] AS u '
              'ON a.UserId=u.UserId '
-             'INNER JOIN [Survey_Sample_A19].[dbo].[Question] As q '
+             'INNER JOIN [Survey_Sample_A19].[dbo].[Question] AS q '
              'ON a.QuestionId=q.QuestionId '
-             'INNER JOIN [Survey_Sample_A19].[dbo].Survey As s '
+             'INNER JOIN [Survey_Sample_A19].[dbo].[Survey] AS s '
              'ON a.SurveyId=s.SurveyId')
     return query
 
 
-def df_to_csv(df, name):
-    df.to_csv('.\ ' + name + '_view.csv')
-    print("\n[%s_view.csv] saved"%name)
-
-
-def check_view(conn):
+# Is going to decide if we need to create or update or neither the view depending of csv file saved in current directory
+def get_view(conn):
     def new_struct(df):
         df.to_csv("./struct_view.csv", sep=';')
         print("\nSaving new view of all data")
@@ -113,14 +131,18 @@ def check_view(conn):
 
     # make new survey structure dataframe, as string because can differ with type from read_csv even with equal values
     new_struct_df = get_db_struct(conn).astype(str)
-    # compare with old dataframe, update if needed, and return the up to data view
+    # compare with old dataframe, update or create if needed, and return the up to data view
     if path.exists("./struct_view.csv"):
         old_struct_df = pd.read_csv("./struct_view.csv", sep=';',
                                     skiprows=[0], names=["SurveyId", "QuestionId", "IsAsked"]).astype(str)
         print("\nView of survey structure already saved:")
         if old_struct_df.equals(new_struct_df):
-            print("\nView of survey structure hasn't been updated\nLoading previous view of all data")
-            all_survey_df = pd.read_csv("./saved_view.csv", sep=';').iloc[:, 1:]
+            if path.exists("./saved_view.csv"):
+                print("\nView of survey structure hasn't been updated\nLoading previous view of all data")
+                all_survey_df = pd.read_csv("./saved_view.csv", sep=';').iloc[:, 1:]
+            else:
+                print("\nMissing saved_view.csv file. Saving")
+                all_survey_df = new_struct(new_struct_df)
         else:
             print("\nView of survey structure has been updated\nSaving the new structure")
             all_survey_df = new_struct(new_struct_df)
@@ -131,10 +153,11 @@ def check_view(conn):
 
 
 def main():
-    server = 'DESKTOP-50BQN97\SQL2019'
-    db = 'Survey_Sample_A19'
+    server, db = read_login_db()
+    read_login_db()
     conn = connect_to_db(server, db)
-    check_view(conn)
+    get_view(conn)
+
 
 if __name__ == "__main__":
     main()
